87a9e3a17bbcc8e88adf77f5486b3ebbc25b2aeb
Reuben Thomas <rrt@sc3d.org>
2026-02-10 21:13:17 +0000

    Add missing copyright headers (fix #14)

--- hulot/__main__.py
+++ hulot/__main__.py
@@ -1,7 +1,12 @@
+# (c) 2002-2023 Reuben Thomas (rrt@sc3d.org, https://github.com/rrthomas/hulot)
+# Distributed under the GNU General Public License version 3, or (at
+# your option) any later version.
+
 import re
 import sys
 
 from . import main
+
 
 sys.argv[0] = re.sub(r"__main__.py$", "hulot", sys.argv[0])
 main()
